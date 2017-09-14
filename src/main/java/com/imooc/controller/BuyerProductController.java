2b15a4067042419c4e6dc485318d6aa57444795b
wujianchuan <wujianchuan@shinow.com>
2017-09-14 20:46:49 +0800

    列表查看接口完成。

--- src/main/java/com/imooc/controller/BuyerProductController.java
+++ src/main/java/com/imooc/controller/BuyerProductController.java
@@ -3,11 +3,20 @@ package com.imooc.controller;
 import com.imooc.VO.ProductInfoVO;
 import com.imooc.VO.ProductVO;
 import com.imooc.VO.ResultVO;
+import com.imooc.dataobject.ProductCategory;
+import com.imooc.dataobject.ProductInfo;
+import com.imooc.service.CategoryService;
+import com.imooc.service.ProductService;
+import com.imooc.utils.ResultVOUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 买家商品
@@ -18,16 +27,40 @@ import java.util.Arrays;
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
 
+    private final ProductService productService;
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public BuyerProductController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/list")
     public ResultVO list() {
-        ResultVO resultVO = new ResultVO();
-        ProductVO productVO = new ProductVO();
-        ProductInfoVO productInfoVO = new ProductInfoVO();
-        productVO.setProductINfoVOList(Arrays.asList(productInfoVO));
 
-        resultVO.setCode(0);
-        resultVO.setMsg("成功");
-        resultVO.setData(Arrays.asList(productVO));
-        return resultVO;
+        List<ProductInfo> productInfoList = productService.findUppAll();
+        List<Integer> categoryTypeList = productInfoList.stream().map(ProductInfo::getCategoryType).collect(Collectors.toList());
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
+
+        List<ProductVO> productVOList = new ArrayList<>();
+        productCategoryList.forEach(productCategory -> {
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryType(productCategory.getCategoryType());
+            productVO.setCategoryName(productCategory.getCategoryName());
+
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            productInfoList.forEach(productInfo -> {
+                if (productInfo.getCategoryType().equals(productCategory.getCategoryType())) {
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo, productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            });
+            productVO.setProductINfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        });
+        return ResultVOUtil.success(productVOList);
     }
 }
